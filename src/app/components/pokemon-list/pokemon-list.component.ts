cc3f5dd6adfde7f76f86478adfeb87deca4bca9f
Osvani Colina <osvacolijim@gmail.com>
2021-01-21 09:08:25 -0400

    Refactor a bit pokemon list component

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -50,7 +50,7 @@ export class PokemonListComponent implements OnInit {
       .subscribe( data => { 
         this.pokemonList = data;
         this.loading = false;
-        this.getPokemonsToShow(this.pokemonList.slice(0,25));
+        this.getPokemonsToShow(this.pokemonList.slice(0,24));
       }, (errorService) => {
         this.messageError = errorService.error.error.message;
         this.error = true;
@@ -64,8 +64,8 @@ export class PokemonListComponent implements OnInit {
     let pokemonFiltered = this.pokemonList.filter( (pokemon)=>{
       return pokemon.name.indexOf(term.toLocaleLowerCase()) !== -1;
     });
-    if(pokemonFiltered.length > 25){
-      pokemonFiltered = pokemonFiltered.slice(0,25);
+    if(pokemonFiltered.length > 24){
+      pokemonFiltered = pokemonFiltered.slice(0,24);
     }
     this.getPokemonsToShow(pokemonFiltered);
     if(pokemonFiltered.length == 0){
@@ -78,7 +78,7 @@ export class PokemonListComponent implements OnInit {
     }
   }
 
-  private getPokemonsToShow(pokemonList: any[]){ //We show max only 25 elements, for now it doesnt have pagination... so we limit this because API have some request's limit
+  private getPokemonsToShow(pokemonList: any[]){ //We show max only 24 elements, for now it doesnt have pagination... so we limit this because API have some request's limit
     this.pokemonsToShow = [];
     pokemonList.forEach((pokemon)=>{
       this.pokeApiService.getPokemonByUrl(pokemon.url)
@@ -87,12 +87,8 @@ export class PokemonListComponent implements OnInit {
           //We get pokemon's chain evolution
           this.pokeApiService.getPokemonSpecies(data['id'])
             .subscribe(pokemonData => {
-              if(pokemonData['evolves_from_species']){
-                data['have_evolve_from'] = true;
-              }else{
-                data['have_evolve_from'] = false;
-              }
-              return data['evolve_from'] = pokemonData['evolves_from_species']
+              data['have_evolve_from'] = pokemonData['evolves_from_species']? true: false;
+              return data['evolve_from'] = pokemonData['evolves_from_species'];
             });
 
           //We sort at the last request
